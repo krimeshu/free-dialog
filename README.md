ddee4a9b68f187dcf4cb51812d7cc074adb7962d
Moonshell <krimeshu@gmail.com>
2015-05-13 10:32:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,380 @@
+# Free Dialog 组件说明文档
+
+
+## I. 简介篇
+
+一款主要用于移动端浏览器的简单弹窗插件，使用原生JavaScript实现。默认提供气泡、提示框、询问框三种常用对话框，还可以自己实例化新的弹窗对象，并轻松自定义新的皮肤样式。
+
+## II. 基础篇
+
+### 1. `FreeDialog.tipsDialog`的使用
+
+```js
+/**
+ * FreeDialog.tips(content, time);
+ * 弹出一个没有标题栏也没有底部按键的气泡框，一段时间后自动消失
+ * @param content 气泡内的文本内容、DOM或二者的数组
+ * @param time 自动消失的时间（毫秒，缺省时为2000毫秒）
+ */
+
+// 例1：弹出一个气泡，两秒后自动关闭，点击周围也可关闭
+FreeDialog.tips('两秒后我会自动关闭');
+
+// 例2：弹出一个两行文本的气泡，三秒后自动关闭
+FreeDialog.tips(['三秒后我会自动关闭', '这是第二行内容'], 3000);
+```
+
+### 2. `FreeDialog.alertDialog`的使用
+
+```js
+/**
+ * FreeDialog.alert(content);
+ * 弹出一个带有确定键的对话框，点击确定后对话框关闭
+ * @param content 对话框内的文本内容、DOM或二者的数组
+ */
+ 
+// 例3：弹出一个对话框，点击确定或右上角X键可关闭
+FreeDialog.alert('点击确定可以关闭');
+
+// 例4：弹出一个对话框，其中第一行为文本，第二行为页面上某个元素
+var dom = document.getElementById('something');
+FreeDialog.alert(['你好啊！', dom]);
+```
+
+### 3. `FreeDialog.askDialog`的使用
+
+```js
+/**
+ * FreeDialog.ask(content, yesCallback, noCallback);
+ * 弹出一个带有确定键的对话框，点击确定或取消后执行对应回调函数
+ * @param content 对话框内的文本内容、DOM或二者的数组
+ * @param yesCallback 点击确定时的回调函数
+ * @param noCallback 点击取消时的回调函数
+ */
+
+// 例5：弹出一个询问框，点取消时阻止关闭，点确定时才可关闭
+FreeDialog.ask('你确定要关闭这个对话框吗？', 
+    function(){         // 点击确定键的回调函数
+        FreeDialog.tips('关闭了。');
+    }, 
+    function(){         // 点击取消键的回调函数
+        FreeDialog.tips('关闭被阻止。');
+        return false;   // 返回false可阻止默认关闭行为
+    }
+);
+```
+
+# III. 扩展篇
+
+### 1. 实例化新的自定义对话框
+
+`FreeDialog`内置的三个对话框都是`FreeDailog`类的对象，通过实例化新的`FreeDialog`对象，配置不同的属性即可增加新的自定义对话框了。
+
+构造函数接受一个初始化参数`options`，参数字段说明如下：
+
+```js
+var options = {
+	title: '提示',			// 对话框标题（可缺省，缺省时默认不显示标题栏）
+	content: '对话框内容',	// 对话框内的文本内容、DOM或两者的数组
+	footBtns: {				// 底部按键对象或数组（可缺省，缺省时不显示底部按键栏）
+		name: '确定',			// 按键文本内容
+		extraClass: null,		// 按键额外的CSS Class（可缺省）
+		click: function (e) {}	// 按键点击时的回调函数（函数内this指向当前FreeDialog对象）
+	},
+	lock: true,				// 对话框出现时是否显示底部黑色遮罩，并阻止拖动屏幕操作（可缺省，默认为true）
+	withCloseBtn: true,		// 右上角是否显示关闭键（可缺省，默认为true）
+	clickAroundClose: true,	// 点击对话框四周是否关闭对话框（可缺省，默认为false）
+	skin: 'skin-orange'		// 默认皮肤的className（可缺省）
+};
+var dialog = new FreeDialog(options);		// 实例化对话框对象
+```
+
+### 2. 修改对话框对象的属性
+
+通过上面实例化得到的对话框对象`dialog`，我们可以做一些操作来修改已有的属性，常用操作如下：
+
+```js
+// 关闭按键相关
+dialog.hideCloseBtn();				// 隐藏对话框关闭按键
+dialog.showCloseBtn();				// 显示对话框关闭按键
+```
+
+```js
+// 背景遮罩相关
+dialog.lock();						// 锁定对话框背景遮罩
+dialog.unblock();					// 解除对话框背景遮罩
+```
+
+```js
+// 标题相关
+dialog.setTitle('新的标题');		// 修改对话框标题文本内容
+dialog.hideTitle();					// 隐藏对话框标题栏
+dialog.showTitle();					// 显示对话框标题栏
+```
+
+```js
+// 内容相关
+dialog.setContent('新的内容');		// 修改对话框内容，与初始化时一样，可为需要设置的文本内容、DOM或两者的数组
+```
+
+```js
+// 底部按键栏相关
+var newBtns = [
+	{
+		name: '按键1',
+		click: function(e) {}
+	},
+	{
+		name: '按键2',
+		click: function(e) {}
+	}
+];
+
+dialog.setFootBtns(newBtns);		// 修改对话框底部按键
+dialog.hideFooter();				// 隐藏对话框底栏和内部按键
+dialog.showFooter();				// 显示对话框底栏和内部按键
+```
+
+```js
+// 皮肤相关
+dialog.setSkin();					// 设置对话框皮肤（为空时撤销现有皮肤）
+```
+
+```js
+// 呼出显示、收起关闭相关
+dialog.show();						// 呼出显示对话框
+window.setTimeout(function(){
+	dialog.close();					// 收起关闭对话框
+}, 3000);
+```
+
+内置的三个种对话框对象`FreeDialog.tipsDialog`, `FreeDialog.alertDialog`, `FreeDialog.askDialog`也可以通过上述方法进行操作。
+
+### 3. 为对话框对象增加事件监听器
+
+目前`FreeDialog`提供四种类型事件的监听，分别为`beforeclose`、`close`、`beforeopen`和`open`，通过调用对象的`on`方法，传入事件类型和对应回调函数即可绑定。
+
+```js
+// 示例代码：
+dialog.on('beforeclose', function(e) {
+	// To do something.
+});
+```
+
+`beforeclose`事件在对话框即将关闭时被触发，回调函数返回`false`可以阻止事件传给后续的`beforeclose`回调函数，并阻止对话框的关闭。
+
+`close`事件在对话框关闭后被触发，回调函数返回`false`可以阻止事件传给后续的`close`回调函数。
+
+`beforeopen`事件在对话框即将呼出时被触发，回调函数返回`false`可以阻止事件传给后续的`beforeopen`回调函数，并阻止对话框的呼出。
+
+`open`事件在对话框关闭后被触发，回调函数返回`false`可以阻止事件传给后续的`open`回调函数。
+
+### 4. 添加新的对话框皮肤
+
+`FreeDialog`的`CSS`样式是使用`SASS`编写的，可以通过修改源码中的`SCSS`重新编译，或直接修改需要的`CSS`文件来添加新的对话框皮肤样式。
+
+如增加一个名为`skin-gray`的皮肤样式，可以通过添加`.dialog-wrapper.skin-gray`及其下子节点（主要是`.dialog-container`、`.dialog-header`、`.dialog-body`、`.dialog-footer`和`dialog-btn`）的新样式来进行皮肤定制。
+
+# IV. 参考篇：
+
+### 1. 内置对话框的实现
+
+```js
+/**
+ * 气泡对话框的创建
+ * @type {FreeDialog}
+ */
+FreeDialog.tipsDialog = new FreeDialog({
+    content: '气泡',
+    withCloseBtn: false,
+    clickAroundClose: true
+});
+/**
+ * 弹出一个没有标题栏也没有底部按键的气泡框，一段时间后自动消失
+ * @param content 气泡内的文本内容、DOM或二者的数组
+ * @param time 自动消失的时间（毫秒，缺省时为2000毫秒）
+ */
+FreeDialog.tips = function (content, time) {
+    time = time === undefined ? 2000 : time;
+    FreeDialog.tipsDialog.setContent(content);
+    FreeDialog.tipsDialog.show(time);
+    if (typeof(time) == 'number' && time > 0) {
+        if (FreeDialog.tipsDialog._closeTimeout) {
+            window.clearTimeout(FreeDialog.tipsDialog._closeTimeout);
+        }
+        FreeDialog.tipsDialog._closeTimeout = window.setTimeout(function () {
+            FreeDialog.tipsDialog._closeTimeout = null;
+            FreeDialog.tipsDialog.close();
+        }, time);
+    }
+};
+FreeDialog.on('beforeclose', function(){
+    // 手动关闭对话框时清除已有定时器
+    if (FreeDialog.tipsDialog._closeTimeout) {
+        window.clearTimeout(FreeDialog.tipsDialog._closeTimeout);
+        FreeDialog.tipsDialog._closeTimeout = null;
+    }
+});
+
+/**
+ * 提示对话框的创建
+ * @type {FreeDialog}
+ */
+FreeDialog.alertDialog = new FreeDialog({
+    title: '提示',
+    footBtns: [
+        {
+            name: '确定',
+            click: function () {
+                FreeDialog.alertDialog.close();
+            }
+        }
+    ],
+    lock: true
+});
+/**
+ * 弹出一个带有确定键的对话框，点击确定后对话框关闭
+ * @param content 对话框内的文本内容、DOM或二者的数组
+ */
+FreeDialog.alert = function (content) {
+    FreeDialog.alertDialog.setContent(content);
+    FreeDialog.alertDialog.show(null);
+};
+
+/**
+ * 询问对话框的创建
+ * @type {FreeDialog}
+ */
+FreeDialog.askDialog = new FreeDialog({
+    title: '提示',
+    footBtns: [
+        {
+            name: '取消',
+            extraClass: 'nagtive',
+            click: function (e) {
+                if (typeof(FreeDialog.askDialog.noCallback) == 'function') {
+                    if (FreeDialog.askDialog.noCallback.apply(this, [e]) === false) {
+                        return;
+                    }
+                }
+                this.close();
+            }
+        },
+        {
+            name: '确定',
+            extraClass: 'positive',
+            click: function (e) {
+                if (typeof(FreeDialog.askDialog.yesCallback) == 'function') {
+                    if (FreeDialog.askDialog.yesCallback.apply(this, [e]) === false) {
+                        return;
+                    }
+                }
+                this.close();
+            }
+        }
+    ],
+    lock: true
+});
+FreeDialog.askDialog.on('beforeclose', function (e) {
+    // 关闭对话框前检查回调函数是否阻止关闭
+    if (e && e.target && e.target.hasAttribute('closeBtn') &&
+        typeof(FreeDialog.askDialog.noCallback) == 'function') {
+        return FreeDialog.askDialog.noCallback.apply(this, [e]);
+    }
+});
+/**
+ * 弹出一个带有确定键的对话框，点击确定或取消后执行对应回调函数
+ * @param content 对话框内的文本内容、DOM或二者的数组
+ * @param yesCallback 点击确定时的回调函数
+ * @param noCallback 点击取消时的回调函数
+ */
+FreeDialog.ask = function (content, yesCallback, noCallback) {
+    FreeDialog.askDialog.setContent(content);
+    FreeDialog.askDialog.yesCallback = yesCallback;
+    FreeDialog.askDialog.noCallback = noCallback;
+    FreeDialog.askDialog.show(null);
+};
+```
+
+### 2. 内置皮肤的实现
+
+```scss
+/* SCSS 代码 */
+.dialog-wrapper {
+    &.skin-bubble {
+        .dialog-container {
+            border: 1px solid #EDECEC;
+            @include prefixer(box-shadow, 0 2px 5px #999999, webkit spec);
+        }
+    }
+
+    &.skin-orange {
+        .dialog-btn {
+            &[closeBtn] {
+                background: transparent url(...) no-repeat scroll center center;
+                background-size: 12px 12px;
+                &:active, &.active {
+                    background-color: rgba(black, 0.1);
+                }
+            }
+        }
+
+        .dialog-header {
+            height: 46px;
+            line-height: 46px;
+            padding: 0;
+            background-color: #EF713D;
+            font-size: 15px;
+            text-align: center;
+            color: white;
+        }
+        .dialog-body {
+            padding: 28px 32px;
+        }
+
+        .dialog-footer {
+            .dialog-btn {
+                color: #EB662D;
+                &.nagtive {
+                    color: #999999;
+                }
+                &:active, &.active {
+                    background-color: #F6F6F6;
+                }
+            }
+        }
+    }
+}
+```
+
+```css
+/* CSS代码 */
+  .dialog-wrapper.skin-bubble .dialog-container {
+    border: 1px solid #EDECEC;
+    -webkit-box-shadow: 0 2px 5px #999999;
+    box-shadow: 0 2px 5px #999999; }
+  .dialog-wrapper.skin-orange .dialog-btn[closeBtn] {
+    background: transparent url(...) no-repeat scroll center center;
+    background-size: 12px 12px; }
+    .dialog-wrapper.skin-orange .dialog-btn[closeBtn]:active, .dialog-wrapper.skin-orange .dialog-btn[closeBtn].active {
+      background-color: rgba(0, 0, 0, 0.1); }
+  .dialog-wrapper.skin-orange .dialog-header {
+    height: 46px;
+    line-height: 46px;
+    padding: 0;
+    background-color: #EF713D;
+    font-size: 15px;
+    text-align: center;
+    color: white; }
+  .dialog-wrapper.skin-orange .dialog-body {
+    padding: 28px 32px; }
+  .dialog-wrapper.skin-orange .dialog-footer .dialog-btn {
+    color: #EB662D; }
+    .dialog-wrapper.skin-orange .dialog-footer .dialog-btn.nagtive {
+      color: #999999; }
+    .dialog-wrapper.skin-orange .dialog-footer .dialog-btn:active, .dialog-wrapper.skin-orange .dialog-footer .dialog-btn.active {
+      background-color: #F6F6F6; }
+```
+
+> Edit at: 2015/05/12
+> Version: 1.1
---
+++ source/js/free-dialog.js
@@ -0,0 +1,546 @@
+/**
+ * Created by Grayson Rex on 2015/5/8.
+ * Version: 1.2
+ */
+
+/* 简单弹窗类 ST */
+(function (window) {
+
+    var document = window.document;
+
+    /**
+     * FreeDialog类
+     * @param options 构造参数，包括title、content、cootBtns等字段
+     * @constructor
+     */
+    var FreeDialog = function (options) {
+        var title = options.title || '';
+        var content = options.content || null;
+        var footBtns = options.footBtns || null;
+        var lock = options.lock === undefined ? true : options.lock;
+        var withCloseBtn = options.withCloseBtn === undefined ? true : options.withCloseBtn;
+        var clickAroundClose = options.clickAroundClose;
+        var skin = options.skin || '';
+
+        var _this = this;
+
+        this.DOM = {};
+
+        var wrapper = this.DOM.wrapper = document.createElement('div');
+        var cover = this.DOM.cover = document.createElement('div');
+        var container = this.DOM.container = document.createElement('div');
+        var btnClose = this.DOM.btnClose = document.createElement('a');
+        var header = this.DOM.header = document.createElement('p');
+        var body = this.DOM.body = document.createElement('div');
+        var footer = this.DOM.footer = document.createElement('div');
+
+        wrapper.className = 'dialog-wrapper closed';
+        cover.className = 'dialog-cover';
+        container.className = 'dialog-container';
+        btnClose.className = 'dialog-btn';
+        btnClose.setAttribute('closeBtn', '');
+        header.className = 'dialog-header';
+        body.className = 'dialog-body';
+        footer.className = 'dialog-footer';
+
+        wrapper.appendChild(cover);
+        wrapper.appendChild(container);
+        container.appendChild(btnClose);
+        container.appendChild(header);
+        container.appendChild(body);
+        container.appendChild(footer);
+        document.body.appendChild(wrapper);
+
+        this.setTitle(title);
+        this.setContent(content);
+        this.setFootBtns(footBtns);
+
+        if (withCloseBtn) {
+            this.showCloseBtn();
+        } else {
+            this.hideCloseBtn();
+        }
+        if (title) {
+            this.showTitle();
+        } else {
+            this.hideTitle();
+        }
+        if (footBtns) {
+            this.showFooter();
+        } else {
+            this.hideFooter();
+        }
+        if (lock) {
+            this.lock();
+        } else {
+            this.unlock();
+        }
+        this.clickAroundClose = clickAroundClose;
+        wrapper.addEventListener('click', function (e) {
+            if (e.target == _this.DOM.wrapper || e.target == _this.DOM.cover) {
+                if (_this.clickAroundClose) {
+                    _this.close();
+                }
+            }
+        });
+        this.setSkin(skin);
+
+        btnClose.addEventListener('click', function (e) {
+            _this.close(e);
+        });
+
+        this.callbacks = {};
+
+        var zIndex = parseInt(getComputedStyle(wrapper)['z-index'], 10);
+        if (!FreeDialog._topZIndex || FreeDialog._topZIndex < zIndex) {
+            FreeDialog._topZIndex = zIndex;
+        }
+    };
+    /**
+     * 判断obj是否为数组对象
+     * @param obj 待判断对象
+     * @private
+     */
+    FreeDialog._isArray = function (obj) {
+        return Object.prototype.toString.call(obj) == '[object Array]';
+    };
+    /**
+     * 判断obj是否为HTML元素
+     * @param obj 待判断对象
+     * @returns {boolean} 是否为HTML元素
+     * @private
+     */
+    FreeDialog._isHTMLElement = function (obj) {
+        var str = Object.prototype.toString.call(obj);
+        return str.indexOf('[object HTML') == 0 && str.indexOf('Element]') == str.length - 'Element]'.length;
+    };
+    /**
+     * 显示右上角的关闭按钮
+     */
+    FreeDialog.prototype.showCloseBtn = function () {
+        var _this = this;
+        _this.DOM.btnClose.style.display = 'block';
+        _this.withCloseBtn = true;
+    };
+    /**
+     * 隐藏右上角的关闭按钮
+     */
+    FreeDialog.prototype.hideCloseBtn = function () {
+        var _this = this;
+        _this.DOM.btnClose.style.display = 'none';
+        _this.withCloseBtn = false;
+    };
+    /**
+     * 锁定对话框背景（显示遮罩层）
+     */
+    FreeDialog.prototype.lock = function () {
+        var _this = this;
+        _this.DOM.cover.classList.remove('hide');
+        _this.locked = true;
+    };
+    /**
+     * 解锁对话框背景（隐藏遮罩层）
+     */
+    FreeDialog.prototype.unlock = function () {
+        var _this = this;
+        _this.DOM.cover.classList.add('hide');
+        _this.locked = false;
+    };
+    /**
+     * 清空对话框标题栏
+     */
+    FreeDialog.prototype.clearTitle = function () {
+        var _this = this;
+        _this.DOM.header.innerHTML = '';
+        _this.title = '';
+    };
+    /**
+     * 设置对话框标题栏
+     * @param title 标题文本
+     */
+    FreeDialog.prototype.setTitle = function (title) {
+        var _this = this;
+        _this.clearTitle();
+        if (FreeDialog._isHTMLElement(title)) {
+            _this.DOM.header.appendChild(title);
+        } else {
+            _this.DOM.header.innerHTML = title;
+        }
+        _this.title = title;
+    };
+    /**
+     * 隐藏对话框标题栏
+     */
+    FreeDialog.prototype.hideTitle = function () {
+        var _this = this;
+        _this.DOM.header.style.display = 'none';
+    };
+    /**
+     * 显示对话框标题栏
+     */
+    FreeDialog.prototype.showTitle = function () {
+        var _this = this;
+        _this.DOM.header.style.display = 'block';
+    };
+    /**
+     * 清空对话框内容
+     */
+    FreeDialog.prototype.clearContent = function () {
+        var _this = this;
+        _this.DOM.body.innerHTML = '';
+    };
+    /**
+     * 设置对话框内容
+     * @param content 内容文本/文本数组
+     */
+    FreeDialog.prototype.setContent = function (content) {
+        var _this = this;
+        _this.clearContent();
+        if (content) {
+            if (!FreeDialog._isArray(content)) {
+                content = [content];
+            }
+            for (var i = 0, r; r = content[i]; i++) {
+                var para = document.createElement('p');
+                if (FreeDialog._isHTMLElement(r)) {
+                    para.appendChild(r);
+                } else {
+                    para.innerHTML = r;
+                }
+                _this.DOM.body.appendChild(para);
+            }
+        }
+        _this.content = content;
+    };
+    /**
+     * 清空对话框底部按键
+     */
+    FreeDialog.prototype.clearFootBtns = function () {
+        var _this = this;
+        _this.DOM.footer.innerHTML = '';
+    };
+    /**
+     * 设置对话框底部按键
+     * @param footBtns 底部按键/按键数组
+     */
+    FreeDialog.prototype.setFootBtns = function (footBtns) {
+        var _this = this;
+        _this.clearFootBtns();
+        if (footBtns) {
+            if (!FreeDialog._isArray(footBtns)) {
+                footBtns = [footBtns];
+            }
+            for (var i = 0, b; b = footBtns[i]; i++) {
+                var btn = document.createElement('a');
+                btn.className = b.extraClass ? 'dialog-btn ' + b.extraClass : 'dialog-btn';
+                btn.innerHTML = b.name;
+                (function (b) {
+                    btn.addEventListener('click', function (e) {
+                        b.click.apply(_this, [e]);
+                    });
+                })(b);
+                _this.DOM.footer.appendChild(btn);
+            }
+        }
+        _this.footBtns = footBtns;
+    };
+    /**
+     * 隐藏对话框底栏
+     */
+    FreeDialog.prototype.hideFooter = function () {
+        var _this = this;
+        _this.DOM.footer.classList.add('hide');
+    };
+    /**
+     * 显示对话框底栏
+     */
+    FreeDialog.prototype.showFooter = function () {
+        var _this = this;
+        _this.DOM.footer.classList.remove('hide');
+    };
+    /**
+     * 设置对话框皮肤
+     * @param skin 皮肤class名
+     */
+    FreeDialog.prototype.setSkin = function (skin) {
+        var _this = this;
+        if (_this.skin) {
+            _this.DOM.wrapper.classList.remove(_this.skin);
+        }
+        if (skin) {
+            _this.DOM.wrapper.classList.add(skin);
+        }
+        _this.skin = skin;
+    };
+    /**
+     * 显示对话框
+     * @param options 显示参数，可选字段 closeAfter:多少毫秒后自动关闭
+     */
+    FreeDialog.prototype.show = function () {
+        var _this = this;
+        if (_this.closing) {
+            return;
+        }
+        var res = _this.trigger('beforeopen', [], true, false);
+        if (res === false) {
+            return;
+        }
+
+        var zIndex = (FreeDialog._topZIndex += 10);
+        _this.DOM.wrapper.style.zIndex = zIndex;
+        _this.DOM.cover.style.zIndex = zIndex + 1;
+        _this.DOM.container.style.zIndex = zIndex + 2;
+
+        _this.DOM.wrapper.classList.add('preToShow');
+        _this.DOM.wrapper.classList.remove('closed');
+
+        _this.DOM.wrapper.offsetWidth = _this.DOM.wrapper.offsetWidth;
+
+        _this.DOM.wrapper.classList.remove('preToShow');
+        _this.DOM.wrapper.classList.add('showed');
+
+        _this.trigger('open', [], true, false);
+    };
+    /**
+     * 关闭对话框
+     */
+    FreeDialog.prototype.close = function (e) {
+        var _this = this;
+        var res = _this.trigger('beforeclose', [e], true, false);
+        if (res === false) {
+            return;
+        }
+
+        _this.DOM.wrapper.classList.remove('showed');
+        _this.DOM.wrapper.classList.add('closing');
+        _this.closing = true;
+        window.setTimeout(function () {
+            _this.DOM.wrapper.classList.remove('closing');
+            _this.DOM.wrapper.classList.add('closed');
+            _this.closing = false;
+
+            _this.trigger('close', [e], true, false)
+        }, 200);
+    };
+    /**
+     * 绑定回调函数
+     * @param type 事件类型（beforeopen, open, beforeclose, close）
+     * @param callback 回调函数
+     */
+    FreeDialog.prototype.on = function (type, callback) {
+        var callbacks = this.callbacks[type];
+        if (Object.prototype.toString.call(callbacks) != '[object Array]') {
+            this.callbacks[type] = callbacks = [];
+        }
+        callbacks.push(callback);
+    };
+    /**
+     * 触发回调函数
+     * @param type 事件类型
+     * @param params 事件参数（数组）
+     * @param getFalseRet 回调函数返回值===false时阻止事件继续执行（可选）
+     * @param getTrueRet 回调函数返回值===true时阻止事件继续执行（可选）
+     */
+    FreeDialog.prototype.trigger = function (type, params, getFalseRet, getTrueRet) {
+        var typeCallbacks = this.callbacks[type] || [];
+        for (var i = 0, callback; callback = typeCallbacks[i]; i++) {
+            var res = callback.apply(this, params);
+            if (res === false && getFalseRet) {
+                return res;
+            }
+            if (res === true && getTrueRet) {
+                return true;
+            }
+        }
+    };
+
+
+    window['FreeDialog'] = FreeDialog;
+
+
+    // 创建内置的常用对话框
+    function creatBuiltInDialogs() {
+        /**
+         * 气泡对话框的创建
+         * @type {FreeDialog}
+         */
+        FreeDialog.tipsDialog = new FreeDialog({
+            content: '气泡',
+            withCloseBtn: false,
+            clickAroundClose: true
+        });
+        /**
+         * 弹出一个没有标题栏也没有底部按键的气泡框，一段时间后自动消失
+         * @param content 气泡内的文本内容、DOM或二者的数组
+         * @param time 自动消失的时间（毫秒，缺省时为2000毫秒）
+         */
+        FreeDialog.tips = function (content, time) {
+            time = time === undefined ? 2000 : time;
+            FreeDialog.tipsDialog.setContent(content);
+            FreeDialog.tipsDialog.show(time);
+            if (typeof(time) == 'number' && time > 0) {
+                if (FreeDialog.tipsDialog._closeTimeout) {
+                    window.clearTimeout(FreeDialog.tipsDialog._closeTimeout);
+                }
+                FreeDialog.tipsDialog._closeTimeout = window.setTimeout(function () {
+                    FreeDialog.tipsDialog._closeTimeout = null;
+                    FreeDialog.tipsDialog.close();
+                }, time);
+            }
+        };
+        FreeDialog.tipsDialog.on('beforeclose', function(){
+            // 手动关闭对话框时清除已有定时器
+            if (FreeDialog.tipsDialog._closeTimeout) {
+                window.clearTimeout(FreeDialog.tipsDialog._closeTimeout);
+                FreeDialog.tipsDialog._closeTimeout = null;
+            }
+        });
+
+        /**
+         * 提示对话框的创建
+         * @type {FreeDialog}
+         */
+        FreeDialog.alertDialog = new FreeDialog({
+            title: '提示',
+            footBtns: [
+                {
+                    name: '确定',
+                    click: function () {
+                        FreeDialog.alertDialog.close();
+                    }
+                }
+            ],
+            lock: true
+        });
+        /**
+         * 弹出一个带有确定键的对话框，点击确定后对话框关闭
+         * @param content 对话框内的文本内容、DOM或二者的数组
+         */
+        FreeDialog.alert = function (content) {
+            FreeDialog.alertDialog.setContent(content);
+            FreeDialog.alertDialog.show(null);
+        };
+
+        /**
+         * 询问对话框的创建
+         * @type {FreeDialog}
+         */
+        FreeDialog.askDialog = new FreeDialog({
+            title: '提示',
+            footBtns: [
+                {
+                    name: '取消',
+                    extraClass: 'nagtive',
+                    click: function (e) {
+                        if (typeof(FreeDialog.askDialog.noCallback) == 'function') {
+                            if (FreeDialog.askDialog.noCallback.apply(this, [e]) === false) {
+                                return;
+                            }
+                        }
+                        this.close();
+                    }
+                },
+                {
+                    name: '确定',
+                    extraClass: 'positive',
+                    click: function (e) {
+                        if (typeof(FreeDialog.askDialog.yesCallback) == 'function') {
+                            if (FreeDialog.askDialog.yesCallback.apply(this, [e]) === false) {
+                                return;
+                            }
+                        }
+                        this.close();
+                    }
+                }
+            ],
+            lock: true
+        });
+        FreeDialog.askDialog.on('beforeclose', function (e) {
+            // 关闭对话框前检查回调函数是否阻止关闭
+            if (e && e.target && e.target.hasAttribute('closeBtn') &&
+                typeof(FreeDialog.askDialog.noCallback) == 'function') {
+                return FreeDialog.askDialog.noCallback.apply(this, [e]);
+            }
+        });
+        /**
+         * 弹出一个带有确定键的对话框，点击确定或取消后执行对应回调函数
+         * @param content 对话框内的文本内容、DOM或二者的数组
+         * @param yesCallback 点击确定时的回调函数
+         * @param noCallback 点击取消时的回调函数
+         */
+        FreeDialog.ask = function (content, yesCallback, noCallback) {
+            FreeDialog.askDialog.setContent(content);
+            FreeDialog.askDialog.yesCallback = yesCallback;
+            FreeDialog.askDialog.noCallback = noCallback;
+            FreeDialog.askDialog.show(null);
+        };
+    }
+
+    // 从当前元素到祖先节点中找到第一个包含className的元素
+    function refluxToFind(obj, className) {
+        if (obj.classList && obj.classList.contains(className)) {
+            return obj;
+        } else if (obj.className && new RegExp('(^| )' + className + '( |$)').test(obj.className)) {
+            return obj;
+        } else if (obj.parentNode && obj.parentNode != document.body) {
+            return refluxToFind(obj.parentNode, className);
+        } else {
+            return null;
+        }
+    }
+
+    // 为包含className类的按键元素解决UC浏览器:active伪类不生效的问题
+    function fixActiveForUC(className) {
+        if (window.navigator.userAgent.indexOf('UCBrowser') >= 0) {
+            document.body.addEventListener('touchstart', function (e) {
+                var dialogBtn = refluxToFind(e.target, className);
+                if (dialogBtn) {
+                    dialogBtn.classList.add('active')
+                }
+            });
+            document.body.addEventListener('touchend', function () {
+                var activeBtns = document.querySelectorAll('.' + className + '.active');
+                for (var i = 0, btn; btn = activeBtns[i]; i++) {
+                    btn.classList.remove('active');
+                }
+            });
+        }
+    }
+
+    // 屏幕大小变化时，修改wrapper的尺寸
+    function modWrapperSize(width, height) {
+        var wrappers = document.getElementsByClassName('dialog-wrapper');
+        for (var i = 0, wrapper; wrapper = wrappers[i]; i++) {
+            wrapper.style.width = width + 'px';
+            wrapper.style.height = height + 'px';
+        }
+    }
+
+    // 初始化弹窗相关
+    function initDialog() {
+        document.addEventListener('touchstart', function () {
+        });
+        document.addEventListener('touchmove', function (e) {
+            if (refluxToFind(e.target, 'dialog-wrapper')) {
+                e.preventDefault();
+            }
+        });
+        window.addEventListener('resize', function () {
+            modWrapperSize(window.innerWidth, window.innerHeight);
+        });
+        var testScreenHeight = function () {
+            // 某些安卓浏览器中，地址栏收起、屏幕旋转等情况导致屏幕大小变化时不触发resize，只能自己定时检测
+            if (!testScreenHeight.oldHeight || testScreenHeight.oldHeight != window.innerHeight) {
+                testScreenHeight.oldHeight = window.innerHeight;
+                modWrapperSize(window.innerWidth, window.innerHeight);
+            }
+        };
+        window.setInterval(testScreenHeight, 1000);
+        fixActiveForUC('dialog-btn');
+        creatBuiltInDialogs();
+    }
+
+    window.addEventListener('load', initDialog);
+
+})(window);
+/* 简单弹窗类 ED */
